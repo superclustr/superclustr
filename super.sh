3a5bb325388a2ec06f8928e96e06c0b7423c068b
Robin Röper <rr@linux.com>
2025-02-23 17:21:54 +0100

    Update super.sh

--- super.sh
+++ super.sh
@@ -1,16 +1,25 @@
 #!/bin/bash
 
-# Define the URL to the latest binary
-BINARY_URL="https://archive.superclustr.net/super"
+# Default binary is latest stable release
+BINARY_NAME="super"
 
-# Download the latest version of the binary to a temporary location
-TMP_BINARY=$(mktemp /tmp/super.XXXXXX)
-curl -sSL $BINARY_URL -o "$TMP_BINARY"
+# Check if the first argument is --nightly
+if [[ "$1" == "--nightly" ]]; then
+    BINARY_NAME="super-nightly"
+    shift # Remove --nightly argument
+fi
+
+# Define the URL to the selected binary
+BINARY_URL="https://archive.superclustr.net/${BINARY_NAME}"
+
+# Download the selected binary to a temporary location
+TMP_BINARY=$(mktemp /tmp/${BINARY_NAME}.XXXXXX)
+curl -sSL "$BINARY_URL" -o "$TMP_BINARY"
 
 # Make the binary executable
 chmod +x "$TMP_BINARY"
 
-# Execute the binary with all the passed parameters
+# Execute the binary with the remaining parameters (excluding --nightly)
 "$TMP_BINARY" "$@"
 
 # Clean up the temporary binary after execution
